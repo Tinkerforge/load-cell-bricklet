2fd6a1094cb868b5b9dbdd9cad98bfb6df104795
Olaf Lüke <olaf@tinkerforge.com>
2015-05-06 15:27:46 +0200

    Add PHP examples

---
+++ software/examples/php/ExampleSimple.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('Tinkerforge/IPConnection.php');
+require_once('Tinkerforge/BrickletLoadCell.php');
+
+use Tinkerforge\IPConnection;
+use Tinkerforge\BrickletLoadCell;
+
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = 'XYZ'; // Change to your UID
+
+$ipcon = new IPConnection(); // Create IP connection
+$lc = new BrickletLoadCell(UID, $ipcon); // Create device object
+
+$ipcon->connect(HOST, PORT); // Connect to brickd
+// Don't use device before ipcon is connected
+
+// Get current weight (unit is g)
+$weight = $lc->getWeight() / 1000.0;
+
+echo "Weight: $weight kg\n";
+
+echo "Press key to exit\n";
+fgetc(fopen('php://stdin', 'r'));
+$ipcon->disconnect();
+
+?>
