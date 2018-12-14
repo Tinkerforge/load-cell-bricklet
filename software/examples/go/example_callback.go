768ac8873697b09a1940efde8bdae86ed19bdb92
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:12:26 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/load_cell_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Load Cell Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	lc, _ := load_cell_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	lc.RegisterWeightCallback(func(weight int32) {
+		fmt.Printf("Weight: %d g\n", weight)
+	})
+
+	// Set period for weight receiver to 1s (1000ms).
+	// Note: The weight callback is only called every second
+	//       if the weight has changed since the last call!
+	lc.SetWeightCallbackPeriod(1000)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
---
+++ software/examples/go/example_threshold.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/load_cell_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Load Cell Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	lc, _ := load_cell_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get threshold receivers with a debounce time of 1 second (1000ms).
+	lc.SetDebouncePeriod(1000)
+
+	lc.RegisterWeightReachedCallback(func(weight int32) {
+		fmt.Printf("Weight: %d g\n", weight)
+	})
+
+	// Configure threshold for weight "greater than 200 g".
+	lc.SetWeightCallbackThreshold('>', 200, 0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
