154fd4ec08751acad7dd6d08b2206aa5f2a7fc70
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:56:28 +0100

    rust: Reformat examples.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  load_cell_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, load_cell_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,24 +10,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let lc = LoadCellBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get threshold receivers with a debounce time of 1 second (1000ms).
-		lc.set_debounce_period(1000);
+    // Get threshold receivers with a debounce time of 1 second (1000ms).
+    lc.set_debounce_period(1000);
 
-     let weight_reached_receiver = lc.get_weight_reached_callback_receiver();
+    let weight_reached_receiver = lc.get_weight_reached_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `lc` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for weight_reached in weight_reached_receiver {           
-                		println!("Weight: {} g", weight_reached);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `lc` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for weight_reached in weight_reached_receiver {
+            println!("Weight: {} g", weight_reached);
+        }
+    });
 
-		// Configure threshold for weight "greater than 200 g".
-		lc.set_weight_callback_threshold('>', 200, 0);
+    // Configure threshold for weight "greater than 200 g".
+    lc.set_weight_callback_threshold('>', 200, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
